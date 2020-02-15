8c557008ea37a0682c8351a88c4fa9082d5a8056
Sammye Jones <sajones@ucdavis.edu>
2020-02-15 12:14:58 -0800

    cleaned up name generator and made it exportable

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const router = express.Router();
-
 router.get("/", function (req, res) {
   res.type("text/html");
   res.send("index.html");
-}
-
+});
 module.exports = router;
