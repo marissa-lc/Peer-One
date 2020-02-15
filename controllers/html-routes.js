db3d48036b9933e228ec15861949ec8985e18423
Luke Robinett <lukebootcamp@gmail.com>
2020-02-15 11:18:31 -0800

    Implemented all html routes.

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -6,4 +6,14 @@ router.get("/", function (req, res) {
   res.send("index.html");
 });
 
+router.get("/feed", function (req, res) {
+  res.type("text/html");
+  res.send("displayfeed.html");
+});
+
+router.get("/skills", function (req, res) {
+  res.type("text/html");
+  res.send("skills.html");
+});
+
 module.exports = router;
