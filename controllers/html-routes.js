de57278ae4b90c8dc491f329eac58ed8e36605f5
Luke Robinett <lukebootcamp@gmail.com>
2020-02-14 21:39:33 -0800

    Built root HTML route

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -1,40 +1,9 @@
-var express = require("express");
+const express = require("express");
+const router = express.Router();
 
-var router = express.Router();
-
-var skill = require("../models/skill.js");
-
-// Create all our routes and set up logic within those routes where required.
-router.get("api/skills", function (req, res) {
-  skill.findAll(function (result) {
-    res.json(res);
-  });
+router.get("/", function (req, res) {
+  res.type("text/html");
+  res.send("index.html");
 });
 
-router.post("/api/skills", function (req, res) {
-
-});
-
-router.put("/api/skills/:id", function (req, res) {
-  var condition = "id = " + req.params.id;
-
-  console.log("condition", condition);
-
-  skill.update(
-    {
-      subject: req.body
-    },
-    condition,
-    function (result) {
-      if (result.changedRows === 0) {
-        // If no rows were changed, then the ID must not exist, so 404
-        return res.status(404).end();
-      }
-      res.status(200).end();
-
-    }
-  );
-});
-
-// Export routes for server.js to use.
 module.exports = router;
