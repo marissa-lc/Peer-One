a4bb23828d2875d1b1b009a7c74ec86a007df2ed
Sammye Jones <sajones@ucdavis.edu>
2020-02-15 11:20:33 -0800

    working on export

--- controllers/name-generator.js
+++ controllers/name-generator.js
@@ -1,6 +1,8 @@
 var WordPOS = require('wordpos'),
     wordpos = new WordPOS();
 
+var names = [];
+
 const promise1 = wordpos.randAdjective();
 const promise2 = wordpos.randNoun();
 const promise3 = Math.floor(Math.random() * 100);
@@ -33,6 +35,8 @@ Promise.all([promise1, promise2, promise3]).then(function (values) {
 
     var output = stringOne + stringTwo + values[2];
     console.log(output);
+    names.push(output);
+    console.log(names);
 });
 
 Promise.all([promise4, promise5, promise6]).then(function (values) {
@@ -46,6 +50,8 @@ Promise.all([promise4, promise5, promise6]).then(function (values) {
 
     var output = stringOne + stringTwo + values[2];
     console.log(output);
+    names.push(output);
+    console.log(names);
 });
 
 Promise.all([promise7, promise8, promise9]).then(function (values) {
@@ -59,6 +65,8 @@ Promise.all([promise7, promise8, promise9]).then(function (values) {
 
     var output = stringOne + stringTwo + values[2];
     console.log(output);
+    names.push(output);
+    console.log(names);
 });
 
 Promise.all([promise10, promise11, promise12]).then(function (values) {
@@ -72,6 +80,8 @@ Promise.all([promise10, promise11, promise12]).then(function (values) {
 
     var output = stringOne + stringTwo + values[2];
     console.log(output);
+    names.push(output);
+    console.log(names);
 });
 
 Promise.all([promise13, promise14, promise15]).then(function(values) {
@@ -85,26 +95,9 @@ Promise.all([promise13, promise14, promise15]).then(function(values) {
 
     var output = stringOne + stringTwo + values[2];
     console.log(output);
+    names.push(output);
+    console.log(names);
   });
+  
 
-// function generateName() {
-//     nameList = [];
-//     var i;
-//     for (i = 0; i < 10; i++) {
-//         const adj = wordpos.randAdjective();
-//         const noun = wordpos.randNoun();
-//         const num1 = Math.floor(Math.random() * 100);
-//         const result = adj + noun + num1;
-//         nameList.push(result); 
-//     }
-//     console.log(nameList);
-// };
-
-// generateName();
-
-
-
-// x.then(function (result) {
-//     console.log(result);
-// })
-
+module.exports=names;
