1bedd009cd7752137f22e232a0bdffd24d050213
Sammye Jones <sajones@ucdavis.edu>
2020-02-15 14:13:54 -0800

    files for luke to test

--- db/wordsdb/wordsschema.sql
+++ db/wordsdb/wordsschema.sql
@@ -5,11 +5,11 @@ use words_db;
 create table descriptors (
     ID int not null AUTO_INCREMENT,
     word varchar(255),
-    primary key(ID, word)
+    primary key(ID)
 );
 
 create table animals (
     ID int not null AUTO_INCREMENT,
     animal_name varchar(255),
-    primary key (ID, animal_name)
+    primary key (ID)
 );
