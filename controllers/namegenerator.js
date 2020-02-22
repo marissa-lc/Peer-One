c61093af6e44455988457d2a985c229602ab59ae
Sarah Vivoli <snvivoli@gmail.com>
2020-02-22 15:28:33 -0800

    namegenerator export function removed

--- controllers/namegenerator.js
+++ controllers/namegenerator.js
@@ -1,36 +1,37 @@
 var WordPOS = require('wordpos'),
     wordpos = new WordPOS();
 
-export function getNames() {
+module.exports = {
+    getNames: () => {
 
-    const names = [];
+        const names = [];
 
-    generateName(wordpos.randAdjective(), wordpos.randNoun());
-    generateName(wordpos.randAdjective(), wordpos.randNoun());
-    generateName(wordpos.randAdjective(), wordpos.randNoun());
-    generateName(wordpos.randAdjective(), wordpos.randNoun());
-    generateName(wordpos.randAdjective(), wordpos.randNoun());
+        generateName(wordpos.randAdjective(), wordpos.randNoun());
+        generateName(wordpos.randAdjective(), wordpos.randNoun());
+        generateName(wordpos.randAdjective(), wordpos.randNoun());
+        generateName(wordpos.randAdjective(), wordpos.randNoun());
+        generateName(wordpos.randAdjective(), wordpos.randNoun());
 
-    setTimeout(() => {
-        console.log(names);
-        return names;
-    }, 1000);
+        setTimeout(() => {
+            console.log(names);
+            return names;
+        }, 1000);
 
-    function generateName(promise1, promise2) {
-        Promise.all([promise1, promise2]).then(function (values) {
+        function generateName(promise1, promise2) {
+            Promise.all([promise1, promise2]).then(function (values) {
 
-            var string1 = values[0].toString();
-            var stringOne = string1[0].toUpperCase() +
-                string1.slice(1);
+                var string1 = values[0].toString();
+                var stringOne = string1[0].toUpperCase() +
+                    string1.slice(1);
 
-            var string2 = values[1].toString();
-            var stringTwo = string2[0].toUpperCase() +
-                string2.slice(1);
+                var string2 = values[1].toString();
+                var stringTwo = string2[0].toUpperCase() +
+                    string2.slice(1);
 
-            var output = stringOne + stringTwo + (Math.floor(Math.random() * 100));
-            names.push(output);
-        });
-    };
+                var output = stringOne + stringTwo + (Math.floor(Math.random() * 100));
+                names.push(output);
+            });
+        };
+    }
 };
 
-module.exports = getNames;
