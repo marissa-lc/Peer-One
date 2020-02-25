7a47c7cdc9f03430d6be8c321e6fb70e74423b2b
Sarah Vivoli <snvivoli@gmail.com>
2020-02-24 17:36:10 -0800

    renaming some classes

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,50 +1,20 @@
-const letsGo = $(".lets-go");
-const signUpDiv = $(".sign-up");
-const createAccount = $(".create-account");
-const createPartial = $(".create-partial");
-const continueBtn = $(".continue-one");
-const usernamePartial = $(".username-partial");
 const checkedUsername = $('input[type="radio"]:checked').val();
-const continue2Btn = $(".continue-two");
-const strengthsPartial = $(".strengths-partial");
 const strengths = $(".strength");
-const skillSelection = $(".skill")
+const skills = $(".skill")
 const post = $(".login-post");
 const newPost = $(".new-post");
 const saveResponse = $(".save");
 const logout = $(".logout-icon");
-
-// index.html - sign up for account
-// createAccount.on("click", function (event) {
-//     event.preventDefault();
-//     signUpDiv.hide();
-//     usernamePartial.show();
-// });
-
-// continueBtn.on("click", function (event) {
-//     event.preventDefault();
-//     usernamePartial.hide();
-//     strengthsPartial.show();
-// });
-
-// continue2Btn.on("click", function (event) {
-//     event.preventDefault();
-//     strengthsPartial.hide();
-//     createPartial.show();
-//     console.log(checkedUsername);
-// });
 
 strengths.on("click", function (event) {
     event.preventDefault();
     $(this).attr("style", "background-color: #F2F2F2;");
 });
 
-skillSelection.on("click", function (event) {
+skills.on("click", function (event) {
     event.preventDefault();
     $(this).attr("style", "background-color: #F2F2F2;");
 });
-
-// end sign up for account
 
 post.on("click", function(event) {
     var addPost = {
@@ -70,7 +40,7 @@ newPost.on("click", function (event) {
     var addPost = {
         userId: 1,
         skillId: 1,
-        body: $(".post-body").val().trim()
+        body: $(".new-post-body").val().trim()
     };
 
     console.log (addPost.body);
